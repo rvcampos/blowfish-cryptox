cd418205da7eb6873c6d52952e2dadc81bdc0b12
renanvcampos <renanvcampos@localhost>
2012-09-21 11:57:34 -0300

    Arrumando testes

--- src/test/java/br/com/vectorx/TestCriptografiaISO8859.java
+++ src/test/java/br/com/vectorx/TestCriptografiaISO8859.java
@@ -2,6 +2,7 @@ package br.com.vectorx;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
@@ -16,33 +17,48 @@ public class TestCriptografiaISO8859 {
 	private String senha = "MEUTESTE123";
 	private String senhaCriptografada = null;
 	private String salt = "TesteThisAfter";
+	private String salt2 = "TesteThisBefore";
 	private int cesarCipher;
 	private BlowfishCryptox x;
+	private BlowfishCryptox x2;
 
 	@Before
 	public void setUp() {
 		Random random = new Random(System.currentTimeMillis());
 		cesarCipher = random.nextInt(64);
-		x = BlowfishCryptox.getInstance(salt, cesarCipher, Charset.forName("ISO8859-1"));
+		x = BlowfishCryptox.getInstance(salt, cesarCipher,
+				Charset.forName("ISO-8859-1"));
+		x2 = BlowfishCryptox.getInstance(salt2, cesarCipher,
+				Charset.forName("ISO-8859-1"));
 		assertNotNull(x);
+		assertNotNull(x2);
+		senhaCriptografada = x.crypt(senha);
 	}
 
 	@Test
 	public void criptografia() {
-		senhaCriptografada = x.crypt(senha);
 		assertNotNull(senhaCriptografada);
 		assertFalse(senha.equals(senhaCriptografada));
-		descriptografa();
-		metodoChecaSenha();
 	}
 
+	@Test
 	public void descriptografa() {
 		String senhaDescriptografada = x.decrypt(senhaCriptografada);
 		assertNotNull(senhaDescriptografada);
 		assertEquals(senha, senhaDescriptografada);
 	}
 
+	@Test
 	public void metodoChecaSenha() {
 		assertTrue(x.checaSenha(senha, senhaCriptografada));
+	}
+
+	@Test
+	public void metodoOutraInstancia() {
+		String senhaCriptografada2 = x2.crypt(senha);
+		assertNotSame(senhaCriptografada, senhaCriptografada2);
+		assertFalse(x.checaSenha(senha, senhaCriptografada2));
+		assertTrue(x2.checaSenha(senha, senhaCriptografada2));
+		assertEquals(senha, x2.decrypt(senhaCriptografada2));
 	}
 }
--- src/test/java/br/com/vectorx/TestCriptografiaUTF8.java
+++ src/test/java/br/com/vectorx/TestCriptografiaUTF8.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
+import static org.junit.Assert.assertNotSame;
+
 import java.util.Random;
 
 import org.junit.Before;
@@ -15,33 +17,46 @@ public class TestCriptografiaUTF8 {
 	private String senha = "MEUTESTE123";
 	private String senhaCriptografada = null;
 	private String salt = "TesteThisAfter";
+	private String salt2 = "TesteThisBefore";
 	private int cesarCipher;
 	private BlowfishCryptox x;
+	private BlowfishCryptox x2;
 
 	@Before
 	public void setUp() {
 		Random random = new Random(System.currentTimeMillis());
 		cesarCipher = random.nextInt(64);
 		x = BlowfishCryptox.getInstance(salt, cesarCipher);
+		x2 = BlowfishCryptox.getInstance(salt2, cesarCipher);
 		assertNotNull(x);
+		assertNotNull(x2);
+		senhaCriptografada = x.crypt(senha);
+	}
+	
+	@Test
+	public void criptografia() {
+		assertNotNull(senhaCriptografada);
+		assertFalse(senha.equals(senhaCriptografada));
 	}
 
 	@Test
-	public void criptografia() {
-		senhaCriptografada = x.crypt(senha);
-		assertNotNull(senhaCriptografada);
-		assertFalse(senha.equals(senhaCriptografada));
-		descriptografa();
-		metodoChecaSenha();
-	}
-
 	public void descriptografa() {
 		String senhaDescriptografada = x.decrypt(senhaCriptografada);
 		assertNotNull(senhaDescriptografada);
 		assertEquals(senha, senhaDescriptografada);
 	}
 
+	@Test
 	public void metodoChecaSenha() {
 		assertTrue(x.checaSenha(senha, senhaCriptografada));
+	}
+
+	@Test
+	public void metodoOutraInstancia() {
+		String senhaCriptografada2 = x2.crypt(senha);
+		assertNotSame(senhaCriptografada, senhaCriptografada2);
+		assertFalse(x.checaSenha(senha, senhaCriptografada2));
+		assertTrue(x2.checaSenha(senha, senhaCriptografada2));
+		assertEquals(senha, x2.decrypt(senhaCriptografada2));
 	}
 }
